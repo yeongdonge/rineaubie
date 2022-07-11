cc0ca0ae905c5ba67f9b8d1645bd68f8d6e7ec44
yeongdonge <parrineau@gmail.com>
2022-07-11 21:51:46 +0900

    repo init

---
+++ src/main/java/com/rineaubie/RineaubieApplication.java
@@ -0,0 +1,13 @@
+package com.rineaubie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RineaubieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RineaubieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rineaubie/RineaubieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rineaubie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RineaubieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
