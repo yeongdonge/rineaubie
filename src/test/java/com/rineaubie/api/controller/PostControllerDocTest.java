6803812b8c4af6d1de111d615e8919b25a5b628a
yeongdonge <parrineau@gmail.com>
2022-07-29 19:21:12 +0900

    adoc 커스터마이징

--- src/test/java/com/rineaubie/api/controller/PostControllerDocTest.java
+++ src/test/java/com/rineaubie/api/controller/PostControllerDocTest.java
@@ -21,6 +21,7 @@ import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuild
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.snippet.Attributes.key;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -60,7 +61,7 @@ public class PostControllerDocTest {
                         .accept(APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andDo(document("index",
+                .andDo(document("post-inquiry",
                         pathParameters(
                                 parameterWithName("postId").description("게시글 ID")
                         ),
@@ -90,10 +91,10 @@ public class PostControllerDocTest {
                         .content(json))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andDo(document("index",
+                .andDo(document("post-create",
                         requestFields(
-                                fieldWithPath("title").description("제목"),
-                                fieldWithPath("content").description("내용")
+                                fieldWithPath("title").description("제목").attributes(key("constraint").value("제목 입력")),
+                                fieldWithPath("content").description("내용").optional()
                         )
                 ));
     }
