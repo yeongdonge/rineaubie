37496e05d5363c1c77bb3db9d2d1ecbb068f9477
yeongdonge <parrineau@gmail.com>
2022-07-30 20:33:51 +0900

    CSS 추가

---
+++ front/src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-header>
+    <nav>
+      <!--        <RouterLink to="/">Home</RouterLink>-->
+      <!--        <RouterLink to="/write">글 작성</RouterLink>-->
+      <el-menu mode="horizontal" router>
+        <el-menu-item index="/">Home</el-menu-item>
+        <el-menu-item index="/write">글 작성</el-menu-item>
+      </el-menu>
+    </nav>
+  </el-header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.header {
+  padding: 0;
+  height: 60px;
+}
+</style>
