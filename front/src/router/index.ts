7883d83a5ea4817e23564b4c4ec4ebdd2383037b
yeongdonge <parrineau@gmail.com>
2022-08-14 19:19:00 +0900

    Front 리팩토링

---
+++ front/src/router/index.ts
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: () => import("../views/HomeView.vue"),
+    },
+    {
+      path: "/write",
+      name: "write",
+      component: () => import("../views/WriteView.vue"),
+    },
+    {
+      path: "/read/:postId",
+      name: "read",
+      component: () => import("../views/ReadView.vue"),
+      props: true,
+    },
+    {
+      path: "/edit/:postId",
+      name: "edit",
+      component: () => import("../views/EditView.vue"),
+      props: true,
+    },
+  ],
+});
+
+export default router;
