b847ce24666f77e3c7fb467265ef6aaf8179242e
yeongdonge <parrineau@gmail.com>
2022-09-04 17:16:18 +0900

    Oauth2 Config 구현

---
+++ src/main/java/com/rineaubie/api/config/auth/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.rineaubie.api.config.auth;
+
+import com.rineaubie.api.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final CustomOauth2UserService customOauth2UserService;
+
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/ts/**", "h2-console/**").permitAll()
+                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                    .anyRequest().authenticated()
+                .and()
+                    .logout()
+                        .logoutSuccessUrl("/")
+                .and()
+                    .oauth2Login()
+                    .userInfoEndpoint()
+                        .userService(customOauth2UserService);
+
+        return http.build();
+    }
+}
